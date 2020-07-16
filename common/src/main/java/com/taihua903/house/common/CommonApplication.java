176d8edf4e1da8949bdb0488895bf1a762124624
bjbg666 <315726312@qq.com>
2020-07-16 16:40:36 +0800

    项目初始化文件

---
+++ common/src/main/java/com/taihua903/house/common/CommonApplication.java
@@ -0,0 +1,13 @@
+package com.taihua903.house.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApplication.class, args);
+    }
+
+}
